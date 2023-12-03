e6823c5274cb274965f49bb160a208d67564b3df
Manolis Chatzakis <manosxatzakiss2000@gmail.com>
2023-12-03 11:00:32 +0100

    merged bloom inside set impl

--- 353068/rw_sets.c
+++ 353068/rw_sets.c
@@ -6,6 +6,45 @@
 #include "rw_sets.h"
 #include "dprint.h"
 
+#include <string.h>
+
+bloom_filter_t *bloom_filter_t_create()
+{
+    bloom_filter_t *bf = (bloom_filter_t *)malloc(sizeof(bloom_filter_t));
+    if (!bf)
+    {
+        return NULL;
+    }
+
+    memset(bf->filter, 0, sizeof(bool) * BLOOM_FILTER_SIZE); // is sizeof(bool ok??)
+    return bf;
+}
+
+void bloom_filter_t_destroy(bloom_filter_t *bloom_filter)
+{
+    // filter is statically allocated, no need for free
+    free(bloom_filter);
+}
+
+void bloom_filter_t_add(bloom_filter_t *bloom_filter, uintptr_t address)
+{
+    int index = address % BLOOM_FILTER_SIZE;
+    bloom_filter->filter[index] = true;
+}
+
+bool bloom_filter_t_contains(bloom_filter_t *bloom_filter, uintptr_t address)
+{
+    return bloom_filter->filter[address % BLOOM_FILTER_SIZE];
+}
+
+void bloom_filter_t_print(bloom_filter_t *bloom_filter){
+    for (int i=0; i<BLOOM_FILTER_SIZE; i++){
+        dprint_clog(COLOR_RESET, stdout, "[%d] ",bloom_filter->filter[i]);
+    }
+    dprint_clog(COLOR_RESET, stdout, "\n");
+}
+
+
 set_t *set_t_init()
 {
     set_t *set = (set_t *)malloc(sizeof(set_t));
--- 353068/rw_sets.h
+++ 353068/rw_sets.h
@@ -2,8 +2,24 @@
 
 #include <stdlib.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <stdint.h>
 
-#include "bloom_filter.h"
+#include "globals.h"
+#include "dprint.h"
+
+typedef struct
+{
+    bool filter[BLOOM_FILTER_SIZE];
+} bloom_filter_t;
+
+bloom_filter_t *bloom_filter_t_create();
+void bloom_filter_t_destroy(bloom_filter_t *bloom_filter);
+
+void bloom_filter_t_add(bloom_filter_t *bloom_filter, uintptr_t address);
+bool bloom_filter_t_contains(bloom_filter_t *bloom_filter, uintptr_t address);
+
+void bloom_filter_t_print(bloom_filter_t *bloom_filter);
 
 typedef struct set_node
 {
